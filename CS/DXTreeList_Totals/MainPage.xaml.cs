6b0047f93530ffee57a5d72a553fcab7b4ab7f38
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXTreeList_Totals/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+using DevExpress.Data;
+
+namespace DXTreeList_Totals {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            treeListControl1.ItemsSource = Stuff.GetStuff();
+            treeListControl1.View.ExpandAllNodes();
+            CreateTotal("Age", SummaryItemType.Min);
+            CreateTotal("Age", SummaryItemType.Max);
+            CreateTotal("Age", SummaryItemType.Average);
+        }
+
+        private void CreateTotal(string fieldName, SummaryItemType summaryType) {
+            TreeListSummaryItem total = new TreeListSummaryItem() {
+                FieldName = fieldName,
+                SummaryType =  summaryType,
+                ShowInColumn = fieldName
+            };
+            treeListControl1.TotalSummary.Add(total);
+        }
+    }
+}
---
+++ VB/DXTreeList_Totals/MainPage.xaml.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Data
+
+Namespace DXTreeList_Totals
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			treeListControl1.ItemsSource = Stuff.GetStuff()
+			treeListControl1.View.ExpandAllNodes()
+			CreateTotal("Age", SummaryItemType.Min)
+			CreateTotal("Age", SummaryItemType.Max)
+			CreateTotal("Age", SummaryItemType.Average)
+		End Sub
+
+		Private Sub CreateTotal(ByVal fieldName As String, ByVal summaryType As SummaryItemType)
+			Dim total As New TreeListSummaryItem() With {.FieldName = fieldName, .SummaryType = summaryType, .ShowInColumn = fieldName}
+			treeListControl1.TotalSummary.Add(total)
+		End Sub
+	End Class
+End Namespace
